4b1c55de5f09d798ec4bd5b09f682419089f9040
Logan McNaughton <logan@bacoosta.com>
2020-02-29 21:21:40 -0700

    deadzone tweak

--- configdialog.cpp
+++ configdialog.cpp
@@ -195,12 +195,12 @@ ProfileEditor::ProfileEditor(QString profile)
     buttonLabelDeadzoneValue->setAlignment(Qt::AlignCenter);
     QSlider *sliderDeadzone = new QSlider(Qt::Horizontal);
     sliderDeadzone->setMinimum(0);
-    sliderDeadzone->setMaximum(300);
+    sliderDeadzone->setMaximum(250);
     sliderDeadzone->setTickPosition(QSlider::TicksBothSides);
     sliderDeadzone->setTickInterval(5);
     connect(sliderDeadzone, &QSlider::valueChanged, [=](int value) {
         float percent = value / 10.0;
-        buttonLabelDeadzoneValue->setText(QString::number(percent, 'f', 1));
+        buttonLabelDeadzoneValue->setText(QString::number(percent, 'f', 1) + "%");
     });
 
     layout->addWidget(buttonLabelDeadzone, 9, 0);
